4981da2105ff66921f1bd7b238232eb86da49ef0
ripuranjan-143 <ripuranjan143@gmail.com>
2026-03-24 23:29:22 +0530

    feat(location): setup location slice with geoLocation hook

--- frontend/src/hooks/useGeoLocation.jsx
+++ frontend/src/hooks/useGeoLocation.jsx
@@ -2,6 +2,7 @@ import { getLocationFromCoordinates } from '../services/locationService.js';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { setCurrentAddress, setCurrentCity, setCurrentState } from '../redux/userSlice.js';
+import { setAddress, setLocation } from '../redux/locationSlice.js';
 
 function useGeoLocation() {
   const dispatch = useDispatch();
@@ -17,12 +18,15 @@ function useGeoLocation() {
           const latitude = position.coords.latitude;
           const longitude = position.coords.longitude;
 
+          dispatch(setLocation({ latitude, longitude }));
+
           const location = await getLocationFromCoordinates(latitude, longitude);
           if (!location) return;
 
           dispatch(setCurrentCity(location.city));
           dispatch(setCurrentState(location.state));
           dispatch(setCurrentAddress(location.address));
+          dispatch(setAddress(location.address));
         } catch (error) {
           console.log('GeoLocation error:', error);
         }
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,13 +2,16 @@ import { configureStore } from '@reduxjs/toolkit';
 import userSlice from './userSlice.js';
 import vendorSlice from './vendorSlice.js';
 import cartSlice from './cartSlice.js';
+import locationSlice from './locationSlice.js';
 
 const store = configureStore({
   reducer: {
     user: userSlice,
     vendor: vendorSlice,
     cart: cartSlice,
+    location: locationSlice,
   },
+  devTools: true,
 });
 
 export default store;
